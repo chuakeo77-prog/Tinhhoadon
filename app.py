7826aeb5ce6edb0ff9a42ce6553a01a5913fd125
chuakeo77-prog <chuakeo77@gmail.com>
2026-09-24 13:29:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ menu = {
         "Mì Ý Bò Bằm": 50000, 
         "Burger Gà": 65000, 
         "Salad Trộn": 50000,
-        "Tinh Yêu Cho Em": 9999999999.
+        "Tinh Yêu Cho Em": 9999999999,
         "Bít tết Bò Mỹ": 250000, 
         "Sườn nướng BBQ": 180000, 
         "Cánh gà chiên mắm": 75000, 
